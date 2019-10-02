f2e05923064d33240996076ab509396b6fcc8334
python pete <pythonpete32@gmail.com>
2019-10-02 14:35:09 +0100

    add constructor, newTokens() helper functions

--- contracts/HiveTemplate.sol
+++ contracts/HiveTemplate.sol
@@ -1,10 +1,12 @@
 pragma solidity 0.4.24;
 
 import "@aragon/apps-shared-minime/contracts/MiniMeToken.sol";
+import "@aragon/templates-shared/contracts/BaseTemplate.sol";
 
 
-contract HiveTemplate {
+contract HiveTemplate is BaseTemplate {
     string constant private ERROR_MISSING_TOKEN_CACHE = "TEMPLATE_MISSING_TOKEN_CACHE";
+    string constant private ERROR_MINIME_FACTORY_NOT_PROVIDED = "TEMPLATE_MINIME_FAC_NOT_PROVIDED";
 
     struct TokenCache {
         address owner;
@@ -14,8 +16,34 @@ contract HiveTemplate {
     
     TokenCache tokenCache;
 
+    constructor(DAOFactory _daoFactory, ENS _ens, MiniMeTokenFactory _miniMeFactory, IFIFSResolvingRegistrar _aragonID)
+        BaseTemplate(_daoFactory, _ens, _miniMeFactory, _aragonID)
+        public
+    {
+        _ensureAragonIdIsValid(_aragonID);
+        _ensureMiniMeFactoryIsValid(_miniMeFactory);
+    }
 
-
+    /**
+    * @dev Create two new MiniMe token and cache them for the user
+    * @param _mbrName String with the name for the token used by members in the organization
+    * @param _mbrSymbol String with the symbol for the token used by members in the organization
+    * @param _mbrName String with the name for the token used for merit in the organization
+    * @param _mbrSymbol String with the symbol for the token used for metit in the organization
+    */
+    function newTokens(
+        string memory _mbrName,
+        string memory _mbrSymbol,
+        string memory _mrtName,
+        string memory _mrtSymbol
+    )
+    public returns (MiniMeToken, MiniMeToken)
+    {
+        MiniMeToken mbrToken = _createNonTransferableToken(_mbrName, _mbrSymbol);
+        MiniMeToken mrtToken = _createTransferableToken(_mrtName, _mrtSymbol);
+        _cacheTokens(mbrToken, mrtToken, msg.sender);
+        return (mbrToken, mrtToken);
+    }
 
     // --------------------------- Internal Functions ---------------------------
     function _cacheTokens(MiniMeToken _mbrToken, MiniMeToken _mrtToken, address _owner) internal {
@@ -33,5 +61,21 @@ contract HiveTemplate {
         delete tokenCache.owner;
 
         return (mbrToken, mrtToken);
+    }
+
+    // ***** i didn't use createToken from basetemplate because i coudnt set transferability *****
+    function _createTransferableToken(string memory _name, string memory _symbol) internal returns (MiniMeToken) {
+        require(address(miniMeFactory) != address(0), ERROR_MINIME_FACTORY_NOT_PROVIDED);
+        MiniMeToken token = miniMeFactory.createCloneToken(MiniMeToken(address(0)), 0, _name, 18, _symbol, true);
+        emit DeployToken(address(token));
+        return token;
+    }
+
+    // ***** i didn't use createToken from basetemplate because i coudnt set transferability *****
+    function _createNonTransferableToken(string memory _name, string memory _symbol) internal returns (MiniMeToken) {
+        require(address(miniMeFactory) != address(0), ERROR_MINIME_FACTORY_NOT_PROVIDED);
+        MiniMeToken token = miniMeFactory.createCloneToken(MiniMeToken(address(0)), 0, _name, 0, _symbol, false);
+        emit DeployToken(address(token));
+        return token;
     }
 }
