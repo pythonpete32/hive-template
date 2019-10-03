bd4c466cffd3c5bc092e3a850d09919ab20ae612
python pete <pythonpete32@gmail.com>
2019-10-03 10:26:10 +0100

    add newTokenAndInstance(), <--overloaded

--- contracts/HiveTemplate.sol
+++ contracts/HiveTemplate.sol
@@ -12,6 +12,8 @@ contract HiveTemplate is BaseTemplate {
     string constant private ERROR_BAD_HOLDERS_STAKES_LEN = "COMPANY_BAD_HOLDERS_STAKES_LEN";
     string constant private ERROR_BAD_VOTE_SETTINGS = "COMPANY_BAD_VOTE_SETTINGS";
 
+    uint64 constant PCT64 = 10 ** 16;
+
     struct TokenCache {
         address owner;
         MiniMeToken mbrToken;
@@ -26,6 +28,59 @@ contract HiveTemplate is BaseTemplate {
     {
         _ensureAragonIdIsValid(_aragonID);
         _ensureMiniMeFactoryIsValid(_miniMeFactory);
+    }
+
+    function newTokensAndInstance() external {
+        uint64[3] memory voteSettings = [uint64(50 ** 16), uint64(50 ** 16), uint64(259200)];
+        address[] memory holders;
+        uint256[] memory stakes;
+
+        holders[0] = msg.sender;
+        stakes[0] = uint256(1 ** 18);
+
+        newTokensAndInstance(
+            "BEE Token",
+            "BEE",
+            "HONEY Token",
+            "HONEY",
+            "1Hive",
+            holders,
+            stakes,
+            voteSettings,
+            voteSettings,
+            voteSettings
+        );
+    }
+
+    /**
+    * @dev Create two new MiniMe token and deploy a 1hive DAO.
+    * @param _mbrName String with the name for the token used by members in the organization
+    * @param _mbrSymbol String with the symbol for the token used by members in the organization
+    * @param _mrtName String with the name for the token used as merit in the organization
+    * @param _mrtSymbol String with the symbol for the token used as merit in the organization
+    * @param _id String with the name for org, will assign `[id].aragonid.eth`
+    * @param _holders Array of member token holder addresses
+    * @param _stakes Array of token merit stakes for member token holders holders (token has 18 decimals, multiply token amount `* 10^18`)
+    * @param _mbrVotingSettings Array of [supportRequired, minAcceptanceQuorum, voteDuration] to set up the member voting app of the organization
+    * @param _mrtVotingSettings Array of [supportRequired, minAcceptanceQuorum, voteDuration] to set up the merit voting app of the organization
+    * @param _dotVotingSettings Array of [_minQuorum, _candidateSupportPct, _voteTime] to set up the dot voting app of the organization
+    */
+    function newTokensAndInstance(
+        string _mbrName,
+        string _mbrSymbol,
+        string _mrtName,
+        string _mrtSymbol,
+        string _id,
+        address[] _holders,
+        uint256[] _stakes,
+        uint64[3] _mbrVotingSettings,
+        uint64[3] _mrtVotingSettings,
+        uint64[3] _dotVotingSettings
+    )
+        public
+    {
+        newTokens(_mbrName, _mbrSymbol,_mrtName, _mrtSymbol);
+        newInstance(_id, _holders, _stakes, _mbrVotingSettings, _mrtVotingSettings);
     }
 
     /**
@@ -74,6 +129,8 @@ contract HiveTemplate is BaseTemplate {
         _transferRootPermissionsFromTemplateAndFinalizeDAO(dao, mbrVoting);
         _registerID(_id, dao);
     }
+
+
 
     // --------------------------- Internal Functions ---------------------------
     function _cacheTokens(MiniMeToken _mbrToken, MiniMeToken _mrtToken, address _owner) internal {
